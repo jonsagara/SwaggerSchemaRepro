595e1876813be4e05bfb7314f17bd55ccae403ba
Jon Sagara <jon@sagarasoftware.com>
2020-10-22 11:19:33 -0700

    Initial commit.

---
+++ src/SwaggerSchemaRepro/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SwaggerSchemaRepro.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
